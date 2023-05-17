28ebd9f46df192280a04fcb437f317a272436e51
HarishFullStack <harishgupta.dev@gmail.com>
2023-05-17 10:10:11 +0530

    integrated mockman backend

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import './App.css';
-import {Routes, Route, NavLink} from 'react-router-dom';
+import "./App.css";
+import {Routes, Route} from "react-router-dom";
+
+import Mockman from "mockman-js";
 import {Home} from "./pages/Home";
 import {Products} from "./pages/Products";
 import {Product} from "./pages/Product";
@@ -8,19 +10,11 @@ import {Wishlist} from "./pages/Wishlist";
 import {Login} from "./pages/Login";
 import {SignUp} from "./pages/SignUp";
 
-
-
 function App() {
   return (
     <div className="App">
-      <NavLink to="/">Home</NavLink> || 
-      <NavLink to="/products">Products</NavLink> || 
-      <NavLink to="/cart">Cart</NavLink> || 
-      <NavLink to="/wishlist">Wishlist</NavLink> || 
-      <NavLink to="/login">Login</NavLink> || 
-      <NavLink to="/signup">SignUp</NavLink>
-
       <Routes>
+        <Route path="/" element={<h1>Hi</h1>}/>
         <Route path="/" element={<Home/>}></Route>
         <Route path="/products" element={<Products/>}></Route>
         <Route path="/product/:id" element={<Product/>}></Route>
@@ -28,9 +22,8 @@ function App() {
         <Route path="/wishlist" element={<Wishlist/>}></Route>
         <Route path="/login" element={<Login/>}></Route>
         <Route path="/signup" element={<SignUp/>}></Route>
+        <Route path="/mockman" element={<Mockman/>}/>
       </Routes>
-
-     
     </div>
   );
 }
